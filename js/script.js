0d2852d2d5722f8807d51ac0a4435d1aaab5c630
BiToc16 <vitaliyvarbanskii@gmail.com>
2026-01-17 21:27:14 +0200

    Add title to index.html

--- js/script.js
+++ js/script.js
@@ -6,4 +6,29 @@ console.log(a);
 console.log(1 + 2);
 console.log(Math.round(4.7)) // 5
 console.log(Math.round(5.6)) // 6
+let name = "Vitaliy";
+console.log(name);
+console.log(`Hello, ${name}`);
+console.log(`4+5 ${4+5}`);
+console.log(`10/3 ${10/3}`);
+console.log (0.1 + 0.2);
+console.log ((0.1*(10) + 0.2*(10))/10);
+console.log (Math.max (10, 20, 30, 5, 15));
+console.log (Math.random ()*(4 - 2) + 2);
+const message = "Hello World";
+console.log (message);
+console.log (message.length);
+console.log (message.toUpperCase());
+console.log (message.toLowerCase());
+let user = {};
+user.name = "Illia";
+user.age = 28;
+user.city = "Kyiv";
+console.log (user);
+delete user.city;
+console.log (user);
+ for (key in user) {
+    console.log (key);
+    console.log (user[key]);
+ }
 
